d92a9b6b71c99bb93eee0008b38338208ede43c9
peron <peron_ap@icloud.com>
2023-10-08 16:36:45 +0100

    fix: /run api not matching

--- server.ts
+++ server.ts
@@ -7,10 +7,10 @@ import { run } from "$/utils/ai.ts";
 const app = new Hono();
 app.route("/assets", assetsRouter);
 
-app.post("/run", async (ctx) => {
-  const { model, input } = await ctx.req.json();
+app.post("/run/*", async (ctx) => {
+  const model = ctx.req.path.slice("/run/".length);
   try {
-    return ctx.json(await run(model, input));
+    return ctx.json(await run(model, ctx.req.raw.body));
   } catch (error) {
     return ctx.json(error?.cause ?? { error });
   }
